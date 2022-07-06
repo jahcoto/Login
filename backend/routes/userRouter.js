1918a9f0a667fa75f4bacb17a8347e04ede8dc8e
jahcoto <jahcoto@gmail.com>
2022-07-06 14:03:15 -0700

    Commit: Controlador Usuarios agregad

--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -2,18 +2,13 @@ import express from "express";
 
 const userRouter = express.Router();
 
+//Importamos el controller del Usuario
+import { getUsers, createUser } from '../controller/userController.js';
+
 //metodo get
-userRouter.get("/", (req, res) => {
-    res.json({
-        "msg": "Desde API/USUARIOS"
-    })
-});
+userRouter.get("/", getUsers);
 
 //metodo post
-userRouter.post("/", (req, res) => {
-    res.send({
-        "msg": "Desde POST/API/USUARIOS"
-    })
-});
+userRouter.post("/", createUser);
 
 export default userRouter;
