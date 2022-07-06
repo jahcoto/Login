2d58fb31e22bf5e32f40f6a2fd040e4d231f35b7
jahcoto <jahcoto@gmail.com>
2022-07-06 09:13:37 -0700

    Commit: Configuracion de la conexion de la BD MongoDB

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,15 @@
 import express from "express";
+import conectarDB from "./config/db.js";
+import dotenv from "dotenv";
 
 const app = express();
 
-app.listen(4000, () => {
-    console.log('Server is running on port 4000');
+dotenv.config();
+
+conectarDB();
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
