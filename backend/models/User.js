22078d296c74a6a757aa46d93e2be945dea0069a
jahcoto <jahcoto@gmail.com>
2022-07-06 10:52:01 -0700

    Commit: Modelo Usuarios agregado

---
+++ backend/models/User.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true
+    },
+    token: {
+        type: String
+    }
+}, 
+    {
+        timestamps: true
+    }
+);
+
+const User = mongoose.model("User", userSchema);
+export default Usuario;
